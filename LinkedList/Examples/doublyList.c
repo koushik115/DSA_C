11b64c525abce0ed2e81b50041b7cabed8f76ecd
kaushikbhat <kaushikbhat115@gmail.com>
2025-07-31 07:25:52 +0530

    feat: implement reverse function to reverse the linked list

--- LinkedList/Examples/doublyList.c
+++ LinkedList/Examples/doublyList.c
@@ -370,3 +370,24 @@ struct node *del(struct node *start, int data) {
     printf("Element %d not found in the list!\n", data);
     return start;
 }
+
+struct node *reverse(struct node *start) {
+    struct node *p1 = NULL, *p2 = NULL;
+
+    p1 = start;
+    p2 = p1->next;
+
+    p1->next = NULL;
+    p1->prev = p2;
+    
+    while(p2 != NULL) {
+        p2->prev = p2->next;
+        p2->next = p1;
+        p1 = p2;
+        p2 = p2->prev;
+    }
+
+    start = p1;
+
+    return start;
+}
