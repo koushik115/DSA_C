edf3b3635b07ca12db45e7185b347fe45fe6a0df
kaushikbhat <kaushikbhat115@gmail.com>
2025-08-29 10:28:31 +0530

    Initial development of validation of paranthesis.

---
+++ Stack/Examples/Applications_of_Stack/parenthesisValidity.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAX 30
+
+int top = -1;
+int stack[MAX];
+
+void push(char item);
+int pop(void);
+int match(char a, char b);
+int check(char exp[]);
+
+int main(void)
+{
+    char exp[MAX];
+    int valid;
+    printf("Enter an algebraic expression: ");
+    gets(exp);
+    valid = check(exp);
+
+    if (valid)
+    {
+        printf("Valid Expression!\n");
+    }
+    else
+    {
+        printf("Invalid Expression!\n");
+    }
+
+    return 0;
+}
+
+int check(char exp[])
+{
+    int i;
+    char temp;
+
+    for (i = 0; i < strlen(exp); i++)
+    {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+            push(exp[i]);
+
+        if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (top == -1)
+            {
+                printf("Right Paranthesis is more than the left Paranthesis");
+                return 0;
+            }
+            else
+            {
+                temp = pop();
+                if (!match(temp, exp[i]))
+                {
+                    printf("Mismatched parenthesis are: ");
+                    printf("%c and %c\n", temp, exp[i]);
+                    return 0;
+                }
+            }
+        }
+    }
+}
