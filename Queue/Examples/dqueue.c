951a3a3f5cb93aa6dcceabdc7e173faccc1d3151
kaushikbhat <kaushikbhat115@gmail.com>
2025-08-29 09:22:26 +0530

    Program to perform Dequeue Operation on a circular array.

---
+++ Queue/Examples/dqueue.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 7
+
+int deque_arr[MAX];
+int front = -1;
+int rear = -1;
+
+void insert_frontEnd(int item);
+void insert_rearEnd(int item);
+int delete_frontEnd(void);
+int delete_rearEnd(void);
+
+void display(void);
+int isEmpty(void);
+int isFull(void);
+
+int main(void)
+{
+    int choice, item;
+
+    while (1)
+    {
+        printf("1. Insert at the front end\n");
+        printf("2. Insert at the rear end\n");
+        printf("3. Delete from the front end\n");
+        printf("4. Delete from the rear end\n");
+        printf("5. Display\n");
+        printf("6. Quit\n");
+
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Input the element for adding in queue: ");
+            scanf('%d', &item);
+            insert_frontEnd(item);
+            break;
+
+        case 2:
+            printf("Input the element for adding in queue: ");
+            scanf('%d', &item);
+            insert_rearEnd(item);
+            break;
+
+        case 3:
+            printf("Element deleted is : %d\n", delete_frontEnd());
+            break;
+
+        case 4:
+            printf("Element deleted is : %d\n", delete_rearEnd());
+            break;
+
+        case 5:
+            display();
+            break;
+
+        case 6:
+            exit(1);
+            break;
+
+        default:
+            printf("Invalid choice!\n");
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void insert_frontEnd(int item)
+{
+    if (isFull())
+    {
+        printf("Queue Overflow!\n");
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = 0;
+        rear = 0;
+    }
+    else if (front == 0)
+    {
+        front = MAX - 1;
+    }
+    else
+    {
+        front -= 1;
+    }
+
+    deque_arr[front] = item;
+}
+
+void insert_rearEnd(int item)
+{
+    if (isFull())
+    {
+        printf("Queue Overflow!\n");
+        return;
+    }
+
+    if (rear == -1)
+    {
+        front = 0;
+        rear = 0;
+    }
+    else if (rear == MAX - 1)
+    {
+        rear = 0;
+    }
+    else
+    {
+        rear += 1;
+    }
+
+    deque_arr[rear] = item;
+}
+
+int delete_frontEnd(void)
+{
+    int item;
+
+    if (isEmpty())
+    {
+        printf("Queue Underflow!\n");
+        exit(1);
+    }
+
+    item = deque_arr[front];
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        if (front == MAX - 1)
+        {
+            front = 0;
+        }
+        else
+        {
+            front += 1;
+        }
+    }
+
+    return item;
+}
+
+int delete_rearEnd(void)
+{
+    int item;
+
+    if (isEmpty())
+    {
+        printf("Queue Underflow!\n");
+        exit(1);
+    }
+
+    item = deque_arr[rear];
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        if (rear == 0)
+        {
+            rear = MAX - 1;
+        }
+        else
+            rear -= 1;
+    }
+
+    return item;
+}
+
+int isFull(void)
+{
+    return ((front == 0 && rear == MAX - 1) || (front == rear + 1));
+}
+
+int isEmpty(void)
+{
+    return (front == -1 & rear == -1);
+}
+
+void display(void)
+{
+    int i;
+
+    if (isEmpty())
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Queue elements: \n");
+
+    i = front;
+    if (front <= rear)
+    {
+        while (i <= rear)
+            printf(" %d", deque_arr[i++]);
+    }
+    else
+    {
+        while (i <= MAX - 1)
+            printf(" %d", deque_arr[i++]);
+
+        i = 0;
+        while (i <= rear)
+            printf(" %d", deque_arr[i++]);
+    }
+
+    printf("\n");
+}
